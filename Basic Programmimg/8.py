bd5f69c78abca9a79956fc735abe0d05d2937ae3
s10singh97 <s10singh97@yahoo.in>
2018-09-11 16:07:33 +0530

    Toggle String

--- Basic Programmimg/8.py
+++ Basic Programmimg/8.py
@@ -3,11 +3,13 @@
 s = input()
 if len(s) > 100 or len(s) < 1:
     sys.exit()
+a = []
 for i in range(0, len(s)):
-    if ord(s[i]) >= 65 or ord(s[i]) <= 90:
-        s[i] = int(ord(s[i]) + 32)
-        s[i] += chr(s[i])
-    if ord(s[i]) >= 97 or ord(s[i]) <= 122:
-        s[i] = int(ord(s[i]) - 32)
-        s[i] += chr(s[i])
-print(s)
+    if ord(s[i]) >= 65 and ord(s[i]) <= 90:
+        a.append(chr(ord(s[i]) + 32))
+    if ord(s[i]) >= 97 and ord(s[i]) <= 122:
+        a.append(chr(ord(s[i]) - 32))
+sr = ""
+for i in a:
+    sr = sr + i
+print(sr)
