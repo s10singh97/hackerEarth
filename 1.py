d5986cdbaddd5871e826d95edeb2adbf6a62b4e9
s10singh97 <s10singh97@yahoo.in>
2018-10-13 12:53:45 +0530

    chefs and semi-primes

--- 1.py
+++ 1.py
@@ -1,25 +1,23 @@
 # Spread the word
 
 t = int(input())
-if t < 1 or t > 1e4:
+if t < 1 or t > 10000:
     sys.exit()
 for  i in range(0, t):
     n = int(input())
-    if n < 2 or n > 1e5:
+    if n < 2 or n > 100000:
         sys.exit()
     a = [int(x) for x in input().split()]
-    for i in range(0, len(a)):
+    for i in range(1, len(a)):
         if a[i] < 0 or a[i] > n:
             sys.exit()
-        if a[0] < 1:
-            sys.exit()
+    if a[0] < 1:
+        sys.exit()
     s = 1
     count = 1
     s += a[0]
     if s >= n:
-        # count += 1
         print(count)
-    # for i in range(0, len(a)):
     else:
         while(s < n):
             for y in range(0, s):
---
+++ 3.py
@@ -0,0 +1,41 @@
+# Chefs and Semi-Primes
+
+t = int(input())
+if t < 1 or t > 200:
+    sys.exit()
+for i in range(0, t):
+    n = int(input())
+    if n < 1 or n > 200:
+        sys.exit()
+    d = []
+    for i in range(2, n+1):
+        c = 0
+        for j in range(1, int(i/2) + 1):
+            if i % j == 0:
+                c += 1
+        if c == 1:
+            d.append(i)
+    # print(d)
+    f = []
+    for i in range(0, int(len(d)/2) + 1):
+        for j in range(i+1, int(len(d)/2) + 1):
+            e = d[i] * d[j]
+            f.append(e)
+    f.pop()
+    # print(f)
+    d.clear()
+    for i in f:
+        if i < n:
+            d.append(i)
+    d.sort()
+    # print(d)
+    flag = 0
+    for i in range(0, len(d)):
+        for j in range(i, len(d)):
+            if d[i] + d[j] == n:
+                flag = 1
+                break
+    if flag == 1:
+        print("YES")
+    else:
+        print("NO")
